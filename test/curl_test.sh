4225836f3b4c04af4682974aed22db30eaf98c46
Zingphoy Han <zingphoy@163.com>
2021-06-27 00:48:15 +0800

    jwt调试完成，跑通基本功能

---
+++ test/curl_test.sh
@@ -0,0 +1,3 @@
+curl 'http://127.0.0.1:8000/hi' \
+  -H 'cookie: access_token_cookie=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyNDcyNTg1OCwianRpIjoiZjAxNjdiZWItMzI0Ny00YjJmLWFkMzctOTQ4MmQ4MmE4MDYxIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6bnVsbCwibmJmIjoxNjI0NzI1ODU4LCJjc3JmIjoiMjExYjBjOGItYWViNi00NDVlLTlhYWItODgxMGUyZWU2MjE4IiwiZXhwIjoxNjI0NzI5NDU4fQ.axJjJJIUfsh3TGJ21T9OIPzpbWqSESFLJelZi1cyiyw' \
+  --compressed
